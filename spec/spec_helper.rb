07c5964b6453d7ed0d39b3436c19dfe4fb69b0a2
Stas SUȘCOV <stas@net.utcluj.ro>
2013-11-28 15:53:45 +0200

    Cleanup spec helper and config files.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,4 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../dummy/config/environment', __FILE__)
 require 'rspec/rails'
-
-Dir[File.join(ENGINE_RAILS_ROOT, 'spec/config/**/*.rb')].each { |f| require f }
-
-RSpec.configure do |config|
-  config.use_transactional_fixtures = false
-end
+Dir[Rails.root.join('../config/**/*.rb')].each {|f| require f}
