dd1ef4a4aad007c948853d375d06971f24b6e27e
Stas SUȘCOV <stas@net.utcluj.ro>
2013-11-28 15:52:36 +0200

    Update the path to the cassettes to relative to our spec directory.

--- spec/config/vcr.rb
+++ spec/config/vcr.rb
@@ -1,12 +1,11 @@
 require 'vcr'
 
 VCR.configure do |c|
-  c.cassette_library_dir = 'spec/cassettes'
-  c.hook_into :faraday
-  c.default_cassette_options = { :record => :new_episodes }
   c.ignore_request do |request|
     # Capybara requests
     URI(request.uri).port == 3999
   end
+  c.hook_into :faraday
+  c.cassette_library_dir = File.expand_path('../../cassettes', __FILE__)
   c.configure_rspec_metadata!
 end
